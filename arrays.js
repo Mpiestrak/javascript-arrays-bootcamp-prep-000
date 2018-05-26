254b86e6f0efaa0107226ca786043c56c3157ee5
Matthew Piestrak <mpiestrak@gmail.com>
2018-05-26 16:16:36 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,23 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+var element = "hershey"
+var 
+function addElementToBeginningOfArray(chocolateBars, element){
+  chocolateBars = [element, ...chocolateBars]
+  return chocolateBars
+}
+
+function destructivelyAddElementToBeginningOfArray(chocolateBars, element){
+  chocolateBars.unshift(element)
+  return chocolateBars
+}
+function addElementToEndOfArray(chocolateBars, element){
+  chocolateBars = [...chocolateBars, element]
+  return chocolateBars
+}
+function destructivelyAddElementToEndOfArray(chocolateBars, element){
+  chocolateBars.push(element)
+  return chocolateBars
+}
+function accessElementInArray(chocolateBars, snickers){
+  console.log(chocolateBars[2])
+}
